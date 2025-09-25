7b58c4bf18f9ef57f58d2339b8211eb85e8a425f
matej2 <11059438+matej2@users.noreply.github.com>
2025-09-25 10:32:52 +0200

    Initial commit

---
+++ src/main/java/com/example/schema_validator/SchemaValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.schema_validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchemaValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchemaValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/schema_validator/SchemaValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schema_validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchemaValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
